7fe1e6fdb3b6424e2992c9fe0de3137d2e36fc74
GuidoRial <rialguido@gmail.com>
2022-01-07 08:36:05 -0300

    added comments to explain the code

--- src/ai.js
+++ src/ai.js
@@ -10,18 +10,22 @@ export class AI extends Player {
     }
 
     generateRandomAttack() {
+        //If it's the computer turn...
         if (this.checkTurn()) {
+            //Create an object representing the coordinates
             let numberObject = { x: undefined, y: undefined };
             while (true) {
+                //Create a random number between 0 and 10 and assign it to x and y
                 let firstNumber = Math.floor(Math.random() * 10);
                 let secondNumber = Math.floor(Math.random() * 10);
                 numberObject.x = firstNumber;
                 numberObject.y = secondNumber;
+                //If the AI didn't attack those coordinates already...
                 if (
                     !this.attackArray.some(
                         (e) => e.x == numberObject.x && e.y == numberObject.y
                     )
-                ) {
+                ) { //...attack
                     this.attackArray.push(numberObject);
                     this.attack(
                         numberObject.x,
